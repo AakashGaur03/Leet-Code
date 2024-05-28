f1ce2c8a38a7c067a77cb5d6889afabf954b5a9f
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2024-05-28 21:36:19 +0530

    Q28)28/05/24

---
+++ May 2024/Q28)GetEqualSubstringsWithinBudget.js
@@ -0,0 +1,49 @@
+// Get Equal Substrings Within Budget
+// Medium
+// Topics
+// Companies
+// Hint
+// You are given two strings s and t of the same length and an integer maxCost.
+
+// You want to change s to t. Changing the ith character of s to ith character of t costs |s[i] - t[i]| (i.e., the absolute difference between the ASCII values of the characters).
+
+// Return the maximum length of a substring of s that can be changed to be the same as the corresponding substring of t with a cost less than or equal to maxCost. If there is no substring from s that can be changed to its corresponding substring from t, return 0.
+
+// Example 1:
+
+// Input: s = "abcd", t = "bcdf", maxCost = 3
+// Output: 3
+// Explanation: "abc" of s can change to "bcd".
+// That costs 3, so the maximum length is 3.
+// Example 2:
+
+// Input: s = "abcd", t = "cdef", maxCost = 3
+// Output: 1
+// Explanation: Each character in s costs 2 to change to character in t,  so the maximum length is 1.
+// Example 3:
+
+// Input: s = "abcd", t = "acde", maxCost = 0
+// Output: 1
+// Explanation: You cannot make any change, so the maximum length is 1.
+
+var equalSubstring = function (s, t, maxCost) {
+  let n = s.length;
+  console.log(n);
+  let ans = -1;
+  let left = 0;
+  let window = 0;
+  for (let right = 0; right < n; right++) {
+    window += Math.abs(s.charCodeAt(right) - t.charCodeAt(right));
+    while (window > maxCost) {
+      window -= Math.abs(s.charCodeAt(left) - t.charCodeAt(left));
+      left++;
+    }
+    ans = Math.max(ans, right - left + 1);
+  }
+  return ans;
+};
+
+(s = "abcd"), (t = "bcdf"), (maxCost = 3);
+// s = "abcd", t = "cdef", maxCost = 3
+// s = "abcd", t = "acde", maxCost = 0
+console.log(equalSubstring(s, t, maxCost));
